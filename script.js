9bb7a3c47f31c32026e7e58d6c91f1e459ca0861
zuzzleberry <azuanic@gmail.com>
2021-02-09 15:46:49 +1100

    first commit

---
+++ script.js
@@ -0,0 +1,127 @@
+
+
+// receive inputs
+
+let buttons = document.querySelector("#buttons");
+let screen = document.querySelector("#screen");
+
+
+
+
+let formula = [];
+let formulaSlices = [[]];
+let sliceCount = 0;
+
+let integerStack = [];
+let operatorStack = [];
+
+let lastResult;
+
+let parser = (operator, x, y) => {
+    switch (operator) {
+        case "+":
+            lastResult = x + y;
+            return;
+        case "-":
+            lastResult = x - y;
+            return;
+        case "*":
+            lastResult = x * y;
+            return;
+        case "/":
+            lastResult = x / y;
+            return;
+    }
+}
+
+buttons.addEventListener("click", (e) => {
+
+    if (e.target.id !== "=") {
+        formula.push(e.target.id);
+        screen.innerHTML = formula.join("")
+
+        if (
+            e.target.id !== "+" &&
+            e.target.id !== "-" &&
+            e.target.id !== "*" &&
+            e.target.id !== "/" 
+        ) {
+            formulaSlices[sliceCount].push(e.target.id)
+        }
+        
+        if (
+            e.target.id === "+" ||
+            e.target.id === "-" ||
+            e.target.id === "*" ||
+            e.target.id === "/"  ) {
+                formulaSlices.push([])
+                sliceCount += 1; 
+                formulaSlices[sliceCount].push(e.target.id)
+                formulaSlices.push([])
+                sliceCount += 1; 
+            }
+            console.log(formulaSlices)
+
+
+    } else if (e.target.id === "=") {
+        
+        for (i = 0; i < formulaSlices.length; i++) {
+
+            if (Number.isInteger(parseInt(formulaSlices[i][0])) === true) {
+                integerStack.push(parseInt(formulaSlices[i].join("")));
+                console.log(integerStack)
+                
+            } else if (
+                formulaSlices[i][0] === "+" ||
+                formulaSlices[i][0] === "-" ||
+                formulaSlices[i][0] === "*" ||
+                formulaSlices[i][0] === "/"
+            ) {
+                operatorStack.push(formulaSlices[i].toString());
+                console.log(operatorStack)
+            }
+        }
+
+        // initial calculation
+        parser(operatorStack[0], integerStack[0], integerStack[1])
+       
+        // stacked calculation
+        if (operatorStack.length > 1) {
+            for (i = 2; i < integerStack.length; i++) {
+                console.log(operatorStack[i - 1])
+                console.log(lastResult)
+                console.log(integerStack[i])
+    
+                parser(operatorStack[i - 1], lastResult, integerStack[i])
+                console.log(lastResult)
+                screen.innerHTML = lastResult;
+
+            }
+         
+        } else {
+            console.log(lastResult)
+            screen.innerHTML = lastResult;
+        }
+
+        
+
+
+        // slice
+        // join strings of numbers
+        // parseInt()
+        // separate at operators
+        // detect type of operators
+        // apply and display calculation
+
+    }
+})
+
+
+
+// addListeners();
+// split and store inputs
+// apply calculations
+// display output
+
+
+
